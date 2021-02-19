b4df4281d1ba6d5aac39f73a78889cfc649f972d
Krum Kuzmov <kkuzmov@gmail.com>
2021-02-19 14:30:18 +0200

    add only public plays to homepage

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     productService.getAll()
         .then(plays =>{
             if(req.user){
-                plays = plays.sort((a, b)=> b.createdAt - a.createdAt);
+                plays = plays.filter(play => play.isPublic === true).sort((a, b)=> b.createdAt - a.createdAt);
                 res.render('user-home', {title: 'Home', plays})
             }else{
                 plays = plays.slice(0, 3).sort((a,b)=> b.usersLiked.length - a.usersLiked.length);
